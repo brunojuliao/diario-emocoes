3eceb1eb281cdb2b6b6cfb4e97def9f7d1f3a1d3
Bruno Julião <brunojuliao.net@gmail.com>
2020-12-04 12:32:40 -0300

    Downgrading vue from 3 to 2 to use bootstrap-vue

--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import './registerServiceWorker'
+import router from './router'
 
-createApp(App)
-  .use(store)
-  .use(router)
-  .mount("#app");
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')
